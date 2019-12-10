ede8b388119227b2f3110e2ee7ffc0ca07ab9bf3
jujubetree <17576059579@163.com>
2019-12-10 21:42:06 +0800

    fiirst commit ,test

---
+++ .idea/src/creationmodel/singletonpattern/HungryStaticBlock.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author ming
+ * @date 2019/12/10 0010 - 20:03
+ */
+public class HungryStaticBlock {
+}
---
+++ .idea/src/creationmodel/singletonpattern/LazyThreadSafe.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author ming
+ * @date 2019/12/10 0010 - 21:22
+ */
+public class LazyThreadSafe {
+}
